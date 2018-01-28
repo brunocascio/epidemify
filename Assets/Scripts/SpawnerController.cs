e53c85a8f44f8b1e6f359c95ed3848f9ece011e3
Antonio <antonioarcuri77@gmail.com>
2018-01-28 14:26:46 -0300

    Spawn de non-infected

---
+++ Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerController : MonoBehaviour {
+
+	public GameObject personaSana;
+	public int maxPeople = 4;
+	public GameObject medic;
+	
+	// Update is called once per frame
+	void Awake () {
+		spawnPeople ();
+		InvokeRepeating ("spawnMedics", 0f, Random.Range(20f, 30f));
+	}
+
+	void spawnPeople()
+	{
+		for (int i = 1; i <= maxPeople; i++) 
+		{
+			Instantiate (personaSana, transform.position, transform.rotation);
+		}
+	}
+
+
+	void spawnMedics()
+	{
+		Instantiate (medic, transform.position, transform.rotation);
+	}
+}
