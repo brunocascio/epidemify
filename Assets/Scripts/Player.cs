92070d192055ccd3c52837ed14f481229a01019a
Antonio <antonioarcuri77@gmail.com>
2018-01-28 02:59:42 -0300

    Zonas de Spawn y comportamiento random de personas

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		//Store the current vertical input in the float moveVertical.
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//Use the two store floats to create a new Vector2 variable movement.
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+
+	}
+}
---
+++ Assets/SpawnGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGenerator : MonoBehaviour {
+	private class Pair {
+		private Pair(float a, float b)
+		{
+			izq=a;der=b;
+		}
+		float izq { get; set;}
+		float der { get; set;}
+	}
+	private class Aristas {
+		private Aristas(Pair p1, Pair p2)
+		{
+			abajo=p1;
+			arriba=p2;
+		}
+
+		Pair abajo{ get; set; }
+		Pair arriba{ get; set; }
+	}
+	List<Aristas> ListaZonas = new List<Aristas>();
+	// Use this for initialization
+	void Awake () {
+		ListaZonas.Add(new Aristas(new Pair(,),new Pair(,));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
